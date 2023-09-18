b94b4a98d8c6cdb9f743751249b603de995e4758
laertLAW <laertsantos29@gmail.com>
2023-09-18 09:10:21 -0300

    parte dois
    falha 1

--- atividade1/ex001.py
+++ atividade1/ex001.py
@@ -1,4 +1,7 @@
-n1 =int( input("digite o primeiro número: "))
-n2 =int( input("digite o segundo número: "))
-soma = n1-n2
-print("Este é o seu resultado: ",soma)
+n1 =int( input(" digite um numero inteiro: "))
+print(type(n1))
+n2 =float( input(" digite um numero real': "))
+print(type(n2))
+n3 =str( input(" digite alguma coisa: "))
+print(type(n3))
+
