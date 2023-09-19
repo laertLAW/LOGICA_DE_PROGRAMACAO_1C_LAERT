725413ad3ad61923581fda81745367e00007b94d
laertLAW <laertsantos29@gmail.com>
2023-09-19 11:26:11 -0300

    002
    metodos

---
+++ atividade1/ex002.py
@@ -0,0 +1,6 @@
+s = input("Digite alguma coisa: ")
+print("tem espaço? ", s.isspace())
+print("é numérico? ",s.isnumeric())
+print("é alfabetico? ",s.isalpha())
+print("é alfabeto e numérico? ",s.isalnum())
+
---
+++ atividade1/ex003.py
@@ -0,0 +1,5 @@
+v1 = int(input("Digite o primeiro múmero: "))
+v2 = int(input("Digite o segundo número: "))
+s = v1 + v2 
+#aqui é feito o calculo da soma de dois números
+
