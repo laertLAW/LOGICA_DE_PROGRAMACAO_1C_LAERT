72ff477b197e8b1a908abdba462bec63d0100c2c
laertLAW <laertsantos29@gmail.com>
2023-10-02 08:19:16 -0300

    + 2
    atividades de sergio

---
+++ atividades do livro/att11.py
@@ -0,0 +1,10 @@
+print("\nTESTANDO HIERARQUIA\n")
+print("\n12 + 5/2 é igual a: ", 12 + 5 / 2)
+print("\n É DIFERENTE DE (12 + 5)/2 que é igual a: ", (12 + 5)/2,", logo / tem hierarquia maior do que + ou -")#continuação
+print("\n\n64**1/4 é igual a: ", 64**1/4)
+print("\nÉ DIFERENTE DE 64**(1/4) que é igual a: ", 64**(1/4),"logo ** tem HIERARQUIA MAIOR do que * ou /")#continuação
+print("\n\n3*7 % 5 é igual a: ", 3*7 % 5)
+print("\nÉ DIFERENTE DE (3 * 7) % 5 que é igual a: ",(3 * 7) % 5," logo % tem HIERARQUIA MAIOR do que *")#continuação
+print("\n\n3 * 7 div 5 é igual a: ", 3*7 / 5)
+print("\nÉ IGUAL a (3 * 7) div 5: ", (3*7) / 5," logo div tem a mesma hierarquia da * ou /")#continuação 
+
