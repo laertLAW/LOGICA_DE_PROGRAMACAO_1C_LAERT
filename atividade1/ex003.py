2f3f8388bd76211a5b0e63a737041566937fc32b
laertLAW <laertsantos29@gmail.com>
2023-10-09 07:36:44 -0300

    prologo-1-site-de-alisson
    aqui ficará o site que foi criado como atividade valendo ponto.

---
+++ atividade1/ex003.py
@@ -0,0 +1,7 @@
+v1 = int(input("Digite o primeiro múmero: "))
+v2 = int(input("Digite o segundo número: "))
+s = v1 + v2 
+#aqui é feito o calculo da soma de dois números
+
+#print("o resultado entre:",v1, " e ",v2," é ",v3)
+print("o valor entre {} e {} o resultado é {}".format (v1,v2,s))
